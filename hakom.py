d475f07fe87b898662fe28d4a40f4f02715b0539
edis <kazaz.edis@gmail.com>
2021-11-13 03:50:06 +0100

    Initial push.

--- hakom.py
+++ hakom.py
@@ -14,7 +14,7 @@ def ocr():
     dataset = (
         dataset.map(m.encode_single_sample,
                     num_parallel_calls=tf.data.AUTOTUNE)
-        .batch(16)  # batch size
+        .batch(1)  # batch size
         .prefetch(buffer_size=tf.data.AUTOTUNE)
     )
 
