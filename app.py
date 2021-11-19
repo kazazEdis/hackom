ff2a549c39785241f1d0788f29711d0c475f6bc8
edis <kazaz.edis@gmail.com>
2021-11-19 03:30:50 +0100

    Search bar improvements

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, make_response, jsonify
 import hakom
 import json
-# from flask_sslify import SSLify
+from flask_sslify import SSLify
 
 app = Flask(__name__)
 # sslify = SSLify(app)
@@ -9,16 +9,18 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def hackom():
-    batch = request.form.get("batch")
     if request.method == "POST":
         msisdn = request.form.get("msisdn")
-        if batch != None:
+        if "," in msisdn:
             results = hakom.batch_operator(msisdn)
             return render_template("mnp_results.html", results=results)
 
-        if len(msisdn) >= 10:
+        elif len(msisdn) >= 10 and "," not in msisdn:
             results = hakom.operator(msisdn)
             return render_template("mnp_results.html", results=results)
+
+        else:
+            return render_template("mnp.html")
 
     else:
         return render_template("mnp.html")
